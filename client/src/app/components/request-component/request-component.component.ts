027183ab9bf8a140bb7ae3f4449cb9df96a0afa7
Seungwoo Kim <seungwookim08@gmail.com>
2019-01-26 18:49:44 -0500

    Make response value more visible

--- client/src/app/components/request-component/request-component.component.ts
+++ client/src/app/components/request-component/request-component.component.ts
@@ -9,7 +9,10 @@ import { OnInit, Component } from '@angular/core';
 export class RequestComponent implements OnInit {
     name = '';
     startDate = '';
-    result: any;
+    resultDate: string[] = [];
+    resultCurrency: any[];
+    currencyKeys = ['AUD','BGN','BRL','CAD','CHF','CNY','CZK','DKK','EUR','GBP','HKD','HRK','HUF','IDR','ILS','INR','ISK','JPY','KRW',
+                    'MXN','MYR','NOK','NZD','PHP','PLN','PLN','RON','RUB','SEK','SGD','THB','TRY','USD','ZAR']
     constructor(private request:RequestService) { }
 
     ngOnInit() {
@@ -20,8 +23,21 @@ export class RequestComponent implements OnInit {
         this.request.startDate = this.startDate;
         this.request.getData().subscribe(
             data => {
-                console.log(data)
-                this.result = data;
+                let stepCurrency = new Array(data.rates.length)
+                let i = 0;
+                for(let key in data.rates){
+                    if (data.rates.hasOwnProperty(key)){
+                        this.resultDate.push(key);
+                        stepCurrency[i] = new Array(this.currencyKeys.length);
+                        for(let currency in this.currencyKeys){
+                            stepCurrency[i][currency] = data.rates[key][this.currencyKeys[currency]];
+                        }
+                        console.log(i);
+                    }
+                    i+= 1;
+
+                }
+                this.resultCurrency = stepCurrency;
             }
         );
     }
