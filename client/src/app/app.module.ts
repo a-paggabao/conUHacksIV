0e0ffe317ae93dc17407422d96e1f58fc2e1080b
apaggabao <a.paggabao@hotmail.com>
2019-01-26 18:10:07 -0500

    added component to show base currency

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
 
 import { D3Service, D3_DIRECTIVES } from './d3';
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
 import { GraphComponent } from './visuals/graph/graph.component';
 import { SHARED_VISUALS } from './visuals/shared';
 import { BallsComponent } from './components/balls/balls.component';
+import { BaseCurrencyComponent } from './components/base-currency/base-currency.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +19,13 @@ import { BallsComponent } from './components/balls/balls.component';
     BallsComponent,
     ...SHARED_VISUALS,
     ...D3_DIRECTIVES,
+    BaseCurrencyComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule
   ],
   providers: [D3Service],
   bootstrap: [AppComponent]
