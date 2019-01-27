e8ed470f9da66b68678a72b4736c88feba6932dc
Paul <paul.musat@mail.concordia.ca>
2019-01-27 07:10:44 -0500

    added converted functiom

--- client/src/app/components/balls/balls.component.ts
+++ client/src/app/components/balls/balls.component.ts
@@ -4,6 +4,7 @@ import { Injectable } from "@angular/core";
 import { Node, Link } from "../../d3";
 import { HttpClient } from "@angular/common/http";
 import { RequestService } from "src/app/service/request.service";
+import { SUPPORTED_CURRENCIES } from "src/app/supportedcurrencies.model";
 
 @Component({
   selector: "app-balls",
@@ -14,34 +15,66 @@ import { RequestService } from "src/app/service/request.service";
 export class BallsComponent implements OnInit {
   nodes: Node[] = [];
   links: Link[] = [];
+  data;
 
   ngOnInit() {
-    let baseCurrency = "EUR";
-    this.service.getCurrentData(baseCurrency).subscribe(data => {
-      console.log(data);
+    let base = "EUR"
+    this.service.getCurrentData(base).subscribe(data => {
+      let ratesArr = new Array();
+      for (let i = 0; i < Object.keys(data.rates).length; i++) {
+        ratesArr.push({code: Object.keys(data.rates)[i], value: Object.values(data.rates)[i]})
+
+        let amount = 500;
+        console.log(ratesArr[i].code, this.convert(amount, ratesArr[i].value))
+      }
+      console.log(ratesArr)
     });
   }
 
+  // async getData(baseCurrency) {
+  //   return await this.service.getCurrentData(baseCurrency).subscribe(data => {
+  //     return data;
+  //   });
+  // }
+
   constructor(private service: RequestService) {
-    const N = APP_CONFIG.N,
-      getIndex = (number: any) => number - 1;
+    let baseCurrency = "EUR";
 
-    this.nodes.push(new Node("1"));
-    
-    /** constructing the nodes array */
-    for (let i = 2; i <= N; i++) {
-      this.nodes.push(new Node(i.toString()));
-    }
+    this.service.getCurrentData(baseCurrency).subscribe(data => {
+      console.log(data.rates);
 
-    for (let i = 1; i <= N; i++) {
-      for (let m = 2; i * m <= N; m++) {
-        /** increasing connections toll on connecting nodes */
-        this.nodes[getIndex(i)].linkCount++;
-        this.nodes[getIndex(i * m)].linkCount++;
+      console.log(Object.keys(data.rates).length);
+      // const N = APP_CONFIG.N,
+      // getIndex = (number: any) => number - 1;
+      const N = Object.keys(data.rates).length,
+        getIndex = (number: any) => number - 1;
+      console.log(N);
 
-        /** connecting the nodes before starting the simulation */
-        this.links.push(new Link(i, i * m));
+      // this.nodes.push(new Node(SUPPORTED_CURRENCIES.indexOf(data.)));
+      this.nodes.push(new Node("1"));
+
+      /** constructing the nodes array */
+      console.log("here");
+      for (let i = 2; i <= N; i++) {}
+      // for(let key of Object.keys(data.rates)) {
+      //   this.nodes.push(new Node(key));
+      //   console.log(key)
+      // }
+
+      for (let i = 1; i <= N; i++) {
+        for (let m = 2; i * m <= N; m++) {
+          /** increasing connections toll on connecting nodes */
+          // this.nodes[getIndex(i)].linkCount++;
+          // this.nodes[getIndex(i * m)].linkCount++;
+
+          /** connecting the nodes before starting the simulation */
+          this.links.push(new Link(i, i * m));
+        }
       }
-    }
+    });
+  }
+
+  convert(amount, next) {
+    return next * amount;
   }
 }
--- client/src/app/service/request.service.ts
+++ client/src/app/service/request.service.ts
@@ -1,26 +1,34 @@
-import { Injectable, Input } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { formatDate } from '@angular/common';
-import { Observable } from 'rxjs';
+import { Injectable, Input } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { formatDate } from "@angular/common";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class RequestService {
   @Input() baseCurrency: string;
-  todayDate = formatDate(new Date(), 'yyyy-MM-dd', 'en');
+  todayDate = formatDate(new Date(), "yyyy-MM-dd", "en");
   now = new Date();
-  startDate = formatDate(new Date(this.now).setFullYear(this.now.getFullYear() - 5),'yyyy-MM-dd', 'en');
-  constructor(private http: HttpClient) {
-  }
+  startDate = formatDate(
+    new Date(this.now).setFullYear(this.now.getFullYear() - 5),
+    "yyyy-MM-dd",
+    "en"
+  );
+  constructor(private http: HttpClient) {}
   getData(): Observable<any> {
     return this.http.get(
-      'https://api.exchangeratesapi.io/history?start_at='+this.startDate+'&end_at='+this.todayDate+'&base='+this.baseCurrency);
-  };
+      "https://api.exchangeratesapi.io/history?start_at=" +
+        this.startDate +
+        "&end_at=" +
+        this.todayDate +
+        "&base=" +
+        this.baseCurrency
+    );
+  }
 
   getCurrentData(base: String): Observable<any> {
-    return this.http.get(
-      `https://api.exchangeratesapi.io/latest/${base}`
-    )
+    return this.http
+      .get(`https://api.exchangeratesapi.io/latest?base=${base}`)
   }
 }
