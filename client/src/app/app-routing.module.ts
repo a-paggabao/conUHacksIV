db1480ca86f6b688faaee3d30d71ce1e4d72dc80
Seungwoo Kim <seungwookim08@gmail.com>
2019-01-26 22:41:59 -0500

    choose the input

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MapComponent } from './components/map/map.component';
 import { BallsComponent } from './components/balls/balls.component';
+import { RequestComponent } from './components/request-component/request-component.component'
 
 const routes: Routes = [
   {path: 'dashboard', component: DashboardComponent},
   {path: 'map', component: MapComponent},
-  {path: 'balls', component: BallsComponent},
+  {path: 'balls', component: RequestComponent},
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
 ];
 
